1ccf2708a0eda25910e23686543e350bbabdf78b
DavidGK1 <dgolubchik.ie@gmail.com>
2023-10-22 00:47:28 +0100

    Added RESTful endpoint to flask app

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/api/items')
+def get_items():
+    items = [{"id": 1, "name": "Item One"}, {"id": 2, "name": "Item Two"}]
+    return jsonify(items)
