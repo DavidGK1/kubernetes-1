5591726e77ec76c1f727675c63e4f6fc9af66c86
DavidGK1 <dgolubchik.ie@gmail.com>
2023-10-25 00:51:10 +0100

    Updated Database connection, added create_user_route function

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from mysql.connector import connect
+
+
 
 app = Flask(__name__)
 
@@ -11,3 +14,15 @@ def hello_world():
 def get_items():
     items = [{"id": 1, "name": "Item One"}, {"id": 2, "name": "Item Two"}]
     return jsonify(items)
+
+def connect_db():
+    return connect(host="localhost", user="root", password="meow", database="my_flask_app")
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (request.form['name'], request.form['email']))
+    db.commit()
+    db.close()
+    return "User created"
