f0fb57e38895d3eecfe3c1cd3578bb3342061173
DavidGK1 <dgolubchik.ie@gmail.com>
2023-11-02 14:09:57 +0000

    Added routes for reading, updating, and deleting users

--- app.py
+++ app.py
@@ -26,3 +26,33 @@ def create_user():
     db.commit()
     db.close()
     return "User created"
+
+@app.route('/get_users', methods=['GET'])
+def get_users():
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    db.close()
+    return str(users)
+
+
+
+@app.route('/update_user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("UPDATE users SET name=%s, email=%s WHERE id=%s", (request.form['name'], request.form['email'], user_id))
+    db.commit()
+    db.close()
+    return f"User with ID {user_id} updated"
+
+
+@app.route('/delete_user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM users WHERE id=%s", (user_id,))
+    db.commit()
+    db.close()
+    return f"User with ID {user_id} deleted"
